cd1da23864281f5d2c64b42424415ebb2a59bd7f
pahalagedara <thilinam4@gmail.com>
2024-09-02 22:48:47 +0530

    dsds

---
+++ src/test/java/com/ceyncfuture/beerserver/BeerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceyncfuture.beerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
